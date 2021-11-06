02dc16f315575e5b4407e03fb97e1a69bb95ab25
Jobie Gamao <jobiemaygamao@gmail.com>
2021-11-06 16:57:09 +0800

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by may on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
